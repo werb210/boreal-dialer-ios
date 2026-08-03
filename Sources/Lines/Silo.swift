dd3521578d6e5ad100e9b775db8dc344255e52a5
werb210 <todd.w@boreal.financial>
2026-08-03 14:36:36 -0600

    Add contacts silo filter

--- Sources/Lines/Silo.swift
+++ Sources/Lines/Silo.swift
@@ -1,6 +1,7 @@
 import Foundation
 
-enum Silo: String, Codable {
+// BOREAL_DIALER_CONTACTS_SILO_v39 - Hashable for the Contacts picker.
+enum Silo: String, Codable, Hashable, CaseIterable {
     case bf
     case bi
     case slf
